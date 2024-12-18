45f620fcd9b4af7b89e7d5425035b3196b29c3d2
CMTrg <minhtruong2605@gmail.com>
2024-12-18 19:29:18 +0700

    Change images src

--- BirthdayCake/src/Images.jsx
+++ BirthdayCake/src/Images.jsx
@@ -4,11 +4,11 @@ function Images({ animationClass }) {
   return (
     <div className={`images-container ${animationClass}`}>
       <p>
-        <img src="../src/assets/BIRTHDAY/Vy/vy1.JPG" alt="" />
-        <img src="../src/assets/BIRTHDAY/Vy/vy2.JPG" alt="" />
-        <img src="../src/assets/BIRTHDAY/Vy/vy3.jpg" alt="" />
-        <img src="../src/assets/BIRTHDAY/Vy/vy4.jpg" alt="" />
-        <img src="../src/assets/BIRTHDAY/Vy/vy5.jpg" alt="" />
+        <img src="./src/assets/BIRTHDAY/Vy/vy1.JPG" alt="" />
+        <img src="./src/assets/BIRTHDAY/Vy/vy2.JPG" alt="" />
+        <img src="./src/assets/BIRTHDAY/Vy/vy3.jpg" alt="" />
+        <img src="./src/assets/BIRTHDAY/Vy/vy4.jpg" alt="" />
+        <img src="./src/assets/BIRTHDAY/Vy/vy5.jpg" alt="" />
        
       </p>
     </div>
